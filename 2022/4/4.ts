8f58d9f02531ab34bae34025fedc6b1e59054550
Jonas Maier <jonas.maier@adabay.rocks>
2023-11-20 09:02:52 +0100

    Solve Day 4 of 2022

---
+++ 2022/4/4.ts
@@ -0,0 +1,15 @@
+import {readLinesFromParam, writeResultToFile} from "../../utils/utils";
+
+const lines = readLinesFromParam()
+    .map(line => line.split(",").map(assignment => assignment.split("-").map(val => parseInt(val))));
+
+const assignmentsWithContainment = lines
+    .filter(([[lhs1, lhs2], [rhs1, rhs2]]) => (lhs1 <= rhs1 && lhs2 >= rhs2) || (rhs1 <= lhs1 && rhs2 >= lhs2))
+    .length;
+
+const assignmentsWithOverlap = lines.filter(([[lhs1, lhs2], [rhs1, rhs2]]) => lhs1 >= rhs1 && lhs1 <= rhs2 || lhs2 >= rhs1 && lhs2 <= rhs2 || (lhs1 <= rhs1 && lhs2 >= rhs2) || (rhs1 <= lhs1 && rhs2 >= lhs2)).length
+
+writeResultToFile({
+    assignmentsWithContainment,
+    assignmentsWithOverlap
+})
